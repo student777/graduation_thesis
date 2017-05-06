7f4d17951ab8878026de59d31c99e55d058da5e9
student777 <darkdakku@gmail.com>
2017-05-07 00:04:48 +0900

    Edit plot: mkdir out

--- plot.py
+++ plot.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 from crawl import get_by_hour
 import numpy
+import os
 
 month = '201701'
+output_path = './out'
 plot_len = 3
 plot_size = plot_len * plot_len
 traffic_list, count = get_by_hour(month)  # about 500 counts
 fig_num = int(count / plot_size)
+
+if not os.path.isdir(output_path):
+    os.makedirs(output_path)
 
 
 for i in range(0, fig_num):
@@ -30,7 +35,7 @@ for i in range(0, fig_num):
     fig.suptitle('지하철 역별 승차/하차 인원수')
     plt.tight_layout()
     plt.subplots_adjust(top=0.9)
-    path_to_save = 'out/traffic_month_%s.png' % i
+    path_to_save = '%s/traffic_month_%s.png' % (output_path, i)
     fig.savefig(path_to_save)
     print('saved %s' % path_to_save)
     plt.close()
