5544bf09da53f290a35307da98684bdd093f5db4
student777 <darkdakku@gmail.com>
2017-05-08 23:11:14 +0900

    Edit database: get location by station info

--- database.py
+++ database.py
@@ -5,9 +5,12 @@ import sqlite3
 def setup_db():
     conn = sqlite3.connect('out/db.sqlite3')
     c = conn.cursor()
-    c.execute('create table stations( name varchar(30), line_num varchar(10), lat real, long real)')
 
-    with open('data/station_location.csv') as csvfile:
+    # create table
+    c.execute('create table stations( name varchar(30), line_num varchar(10), lat real, lng real)')
+
+    # insert data
+    with open('res/station_location.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in list(reader)[1:]:
             station_info = row[1], row[2], row[7], row[8]
@@ -15,3 +18,19 @@ def setup_db():
             conn.commit()
 
     conn.close()
+
+
+def get_location(name, line_num):
+    # check line_num
+    allowed_line_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'E', 'G', 'I', 'I2', 'K', 'KK', 'S', 'SU', 'U']
+    if line_num not in allowed_line_num:
+        print('wrong line_num')
+        return
+
+    # fetch data
+    conn = sqlite3.connect('out/db.sqlite3')
+    c = conn.cursor()
+    c.execute('SELECT lat, lng FROM stations where name=? and line_num=?', (name, line_num))
+    row = c.fetchone()
+    conn.close()
+    return row
